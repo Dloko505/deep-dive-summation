df75a16039a4dd409c9860f06e304153fe3505a6
Dloko505 <Dloko505@gmail.com>
2018-05-21 14:42:36 -0600

    Commit adding Javadoc including author and version number.

--- src/edu/cnm/deepdive/Summation.java
+++ src/edu/cnm/deepdive/Summation.java
@@ -1,7 +1,18 @@
 package edu.cnm.deepdive;
 
+/**
+ * This is the class containing  the main point for the (eventual) RPN calculator.
+ * 
+ * @author De_El_ArE
+ * @version 0.1
+ */
 public class Summation {
 
+  /**
+   * Command-line entry point for RPN interpreter.
+   * 
+   * @param args operands and operators.
+   */
   public static void main(String[] args) {
     int sum = 0;
     for (int i = 0; i < args.length; i++) {
